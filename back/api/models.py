da3966a4c57d6795179c10f111591f8afb0df695
lseonhye <sh133726@naver.com>
2025-02-25 10:01:54 +0800

    back

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Resource(models.Model):
+    campus = models.CharField(max_length=255)
+    resource_code = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    capacity = models.IntegerField(null=True, blank=True)  # NULL 값 허용
+    lecture = models.BooleanField(default=False)
+    tutorial = models.BooleanField(default=False)
+    lab = models.BooleanField(default=False)
+    workshop = models.BooleanField(default=False)
+    resource_status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.resource_code} - {self.description}"
+
+class CourseOffer(models.Model):
+    course_code = models.CharField(max_length=50)
+    faculty_code = models.CharField(max_length=50)
+    session = models.TextField()
+    capacity = models.IntegerField(null=True, blank=True)  # NULL 값 허용
+    min_per_session = models.IntegerField()
+    lecturer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.course_code} - {self.session}"
--- back/config/settings.py
+++ back/config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,6 +79,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 DATABASES = {
     'default': {
